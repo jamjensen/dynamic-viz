2406ad571dfa6e820c891ae3e89965fb7b317968
James Jensen <jamesronaldjensen@gmail.com>
2020-03-01 23:55:27 -0600

    cleanup

--- src/app.js
+++ src/app.js
@@ -5,38 +5,6 @@ const domReady = require('domready');
 import {csv} from 'd3-fetch'
 import './stylesheets/main.css';
 
-// domReady(() => {
-//   // this is just one example of how to import data. there are lots of ways to do it!
-// //   fetch('./data/example.json')
-// //     .then(response => response.json())
-// //     .then(data => myVis(data))
-// //     .catch(e => {
-// //       console.log(e);
-// //     });
-// // });
-
-// domReady(() => {
-//   // this is just one example of how to import data. there are lots of ways to do it!
-//   fetch('./data/example.json')
-//     .then(response => response.json())
-//     .then(data => myVis(data))
-//     .catch(e => {
-//       console.log(e);
-//     });
-// });
-
-
-// domReady(() => {
-//   // this is just one example of how to import data. there are lots of ways to do it!
-//   csv('./data/tst_clean.csv')
-//   // .then(data => console.log(data))
-//   .then(data => makeGrid(data))
-//     .catch(e => {
-//       console.log(e);
-//       console.log('YOOOO')
-//     });
-// });
-
 
 function makeGrid(data) {
   var grid = d3.select("body")
@@ -45,44 +13,25 @@ function makeGrid(data) {
     .attr("class", "grid")
   ;
   console.log(data)
-  var chars = grid
+  var boxes = grid
     .selectAll("div")
     .data(data)
     .enter()
     .filter(function(d){ return d.NUMBER_OF_PERSONS_INJURED >= 0; })
     .append("div")
-    .attr("class", "char")
+    .attr("class", "boxes")
     .style("background-color", "red")
 
   ;
 }
-d3.csv("./data/tst_clean.csv")
+csv("./data/tst_clean.csv")
   .then(data => makeGrid(data))
   .catch(e => {console.log(e);
   });
 
-// d3.csv("/data/tst_clean.csv").then(function(data) {
-//   console.log(data);
-// });
 
-// d3.json("https://gist.githubusercontent.com/andybarefoot/172ebdb29e781a71625753ab02f4920d/raw/ced130d481e3b363fa053bffc37034139eb1e670/chars.json").then(function(data) {
-//   console.log(data.characters);
-// });
+
 
 
 console.log('tst')
 
-// d3.csv("/data/tst_clean.csv").then(function(data) {
-//   grid = d3.select("body")
-//     .append("div")
-//     .attr("id", "grid")
-//     .attr("class", "grid")
-//   ;
-//   chars = grid
-//     .selectAll("div")
-//     .data(data.CRASH_DATE)
-//     .enter()
-//     .append("div")
-//     .attr("class", "char")
-//   ;
-// })

0bbc9a49cd3748a4c3f3b4bf5d34ebd08ff377f0
James Jensen <jamesronaldjensen@gmail.com>
2020-03-06 00:06:04 -0600

    updates to data processing + more grids

--- processing/crash_cleaning.py
+++ processing/crash_cleaning.py
@@ -1,8 +1,83 @@
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import shape, Point
 
-df = pd.read_csv('data/crashes_2019.csv')
-df.columns = df.columns.str.replace(' ', '_')
-df.to_csv('data/crashes_clean.csv')
-tst = df.head(20)
-tst.to_csv('data/tst_clean.csv')
-print('done')
+
+
+
+def make_df():
+
+    df = pd.read_csv('data/crashes_2019.csv')
+    df.columns = df.columns.str.replace(' ', '_')
+    df.to_csv('data/crashes_clean.csv')
+    tst = df.head(20)
+    tst.to_csv('data/tst_clean.csv')
+    print('done')
+    df[(df['NUMBER_OF_CYCLIST_KILLED'] >=1) | (df['NUMBER_OF_CYCLIST_INJURED'] >=1)]\
+        [['NUMBER_OF_PEDESTRIANS_INJURED','NUMBER_OF_PEDESTRIANS_KILLED',\
+        'NUMBER_OF_CYCLIST_INJURED','NUMBER_OF_CYCLIST_KILLED']].to_csv('cyclist-binary.csv')
+
+    df = df[(df['NUMBER_OF_CYCLIST_KILLED'] >=1) | (df['NUMBER_OF_CYCLIST_INJURED'] >=1)].reset_index()
+    df.drop(columns='index',inplace=True) 
+    df = df[~df['LATITUDE'].isnull()] 
+    df = df[df['LONGITUDE'] < 0]
+    df['geometry'] = df.apply(lambda x: Point(x['LONGITUDE'],x['LATITUDE']), axis=1)
+    geo_df = gpd.GeoDataFrame(df) 
+
+
+def make_gdf():
+    bike_paths = gpd.read_file('bike-paths.geojson')
+    bike_paths[['allclasses','geometry']] 
+
+def assign_district(bike_paths, geo_df):
+    
+    # keys = bike_paths['geometry']
+    # values = bike_paths['allclasses']
+    # bike_dict = dict(zip(keys,values))
+
+    points = list(geo_df['geometry'])
+    bike_class = {}
+    
+    path_distance_dict = {}
+    
+    for i, pt in enumerate(points):
+        smallest = 10
+        print(i)
+        poly_count = 0
+        for idx,row in bike_paths.iterrows():
+        # for poly,path_type in bike_dict.items():
+            line = row['geometry']
+            path = row['allclasses']
+            # print(poly)
+            # print(path)
+            # print(pt.within(line))
+            # print(pt.distance(line))
+            if pt.distance(line) < 1e-4:
+                # print('**************************************************************')
+                distance = pt.distance(line)
+                if distance < smallest:
+                    # print('FUCKKKKKKKKK OFFFFFFFFFF')
+                    path_distance_dict[i] = idx
+                    bike_class[i] = path
+                    smallest = distance
+
+
+    df = pd.DataFrame.from_dict(bike_class,orient='index')
+    df.to_csv('bike_path_classes.csv')
+
+    # return bike_class
+
+def plot_single_accident():
+
+    fig, ax = plt.subplots(figsize = (8,8)) 
+
+    gspaths = gpd.GeoSeries(bike_paths.loc[11661,'geometry'])
+    gs = gpd.GeoSeries(geo_df.loc[8,'geometry'])
+
+    bike_paths.plot(ax = ax)
+
+    gs.plot(ax=ax, marker='*', color='red')
+    gspaths.plot(ax=ax, color='red') 
+
+
+
--- src/app.js
+++ src/app.js
@@ -2,36 +2,96 @@
 // import MY_DATA from './app/data/example.json'
 // (I tend to think it's best to use screaming snake case for imported json)
 const domReady = require('domready');
-import {csv} from 'd3-fetch'
+import {csv} from 'd3-fetch';
+import {select} from 'd3-selection';
 import './stylesheets/main.css';
 
 
-function makeGrid(data) {
-  var grid = d3.select("body")
-    .append("div")
-    .attr("id", "grid")
-    .attr("class", "grid")
-  ;
+var grid = select("body")
+  .append("div")
+  .attr("id", "grid")
+  .attr("class", "grid")
+;
+function makeGrid(data, colorFunction) {
   console.log(data)
   var boxes = grid
-    .selectAll("div")
-    .data(data)
+    .selectAll(".boxes")
+    .data(data);
+  boxes
     .enter()
-    .filter(function(d){ return d.NUMBER_OF_PERSONS_INJURED >= 0; })
     .append("div")
     .attr("class", "boxes")
-    .style("background-color", "red")
-
+    .merge(boxes)
+    .style("background-color", colorFunction)
   ;
 }
+// transform agg into this 
+const boundariesExample = [
+{upperBound: 300, color: 'green'},
+{upperBound: 700, color: 'blue'},
+{upperBound: 1000, color: 'red'}
+]
+setTimeout(() => {
+  // makeGrid(getLength(1000), (d, idx) => idx % 2 ? 'blue' : 'red')
+  const colorFunc = (d, idx) => {
+    return boundariesExample.find(boundary => {
+      return idx < boundary.upperBound;
+    }).color
+  }
+  makeGrid(getLength(4000), colorFunc)
+}, 500)
+// UNDO THIS COMMMENT
+// csv("./data/cyclist-binary.csv")
+//   .then(data => makeGrid(data))
+//   .catch(e => {console.log(e);
+//   });
+
+// makeGrid(getLength(4000), d => 'red')
+
+
+const state = {slideIdx: 0};
+const buttons = select('.buttons-container')
+    .selectAll('button')
+    .data([0, 1, 2])
+    .enter()
+    .append('button')
+    .text(d => d)
+    ;
+
+buttons
+
+// domReady(() => {
+//   Promise.all([
+//     csv('./data/cyclist-binary.csv')
+//   ]).then(d => {
+//     const [data] = d;
+//     console.log(data.length)
+//     // app(data, article);
+//   });
+// });
+
+// var length = 5; // user defined length
+
+function getLength(length) {
+  var loop = [];
+  // var length = data;
+  for(var i = 0; i < length; i++) {
+      loop.push(i);
+  }
+  return loop
+}
+var loop = getLength(10)
+console.log(loop)
+
+var tst = [0,1,2,3]
+
 csv("./data/tst_clean.csv")
-  .then(data => makeGrid(data))
+  .then(data => console.log(data[0]["CRASH_DATE"]))
   .catch(e => {console.log(e);
   });
 
+// console.log(loop)
+// console.log([0,1,2].length)
 
-
-
-
-console.log('tst')
+console.log(tst)
 
